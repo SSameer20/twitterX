addd322e399ec78624ae329d10ca1c952ff2e551
Sameer Shaik <sameer.kattubadi@gmail.com>
2024-06-20 02:13:17 +0530

    added register details

--- server/server.js
+++ server/server.js
@@ -1,45 +1,63 @@
-const express = require('express')
-const cors = require('cors')
+const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
-const app = express();
-const bodyParser = require('body-parser');
-// const sql = require('mssql');
+// const bcrypt = require('bcrypt');
 require('dotenv').config();
 
-//DB
-async function connectToMongoDB() {
-    try {
-        // Connect to the MongoDB server
-        await mongoose.connect(uri, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
-        console.log('Connected to MongoDB with mongoose');
-    } catch (err) {
-        console.error('Failed to connect to MongoDB', err);
-    }
-}
+const User = require('./model/userModel'); 
 
-// Middle Ware 
+const app = express();
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json()); 
 
-//Databse Config
+const url = process.env.MONGO_URL;
+const dbName = process.env.DB;
 
+mongoose.connect(url)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch(err => {
+    console.error('Failed to connect to MongoDB', err);
+  });
 
-app.get("/", (req, res) => {
-    res.send("Server is Running")
-})
-
-app.post("/tweet", (req, res) => {
-    const twt = req.body.twt
-    console.log(twt);
-    res.send({ success: "Send Successfully" })
-})
-
-
+  
 
 
-app.listen(3001, (req, res) => {
-    console.log(`Server is Running on http://localhost:3001`)
-})
+  // post routes
+
+  app.post('/user/register', async (req, res) => {
+    const { user, remail, rpassword } = req.body;
+    // console.log(user + " " + remail + " "+ rpassword)
+    
+    try {
+        // Check if the username or email already exists
+        const existingUser = await User.findOne({ $or: [{ user }, { email: remail }] });
+        if (existingUser) {
+            return res.status(400).json({ success: false, message: 'Username or email already exists' });
+        }
+        
+        // Create a new user instance
+        const newUser = new User({ user, email: remail, password : rpassword });
+
+        // Save the user to the database
+        await newUser.save();
+
+        // Respond with the created user object (excluding the password)
+        res.status(201).json({ success: true, user: newUser });
+    } catch (error) {
+        // Handle errors
+        res.status(400).json({ success: false, error: error.message });
+    }
+  });
+
+  // get routes
+
+  app.get("/", (req,res) => {
+    res.send("Server Started")
+  })
+  
+  const PORT = 3001
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
